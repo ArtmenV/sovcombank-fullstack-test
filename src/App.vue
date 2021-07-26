0860faf12a998379a18457abfcb83dc8d9afc4a9
Viliutich Artem <vilyutich.ab@sibvaleo.com>
2021-07-26 14:11:01 +0700

    feature/search-form

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,65 @@
 <template>
   <div>
+    <search-form />
   </div>
 </template>
 
 <script>
+import SearchForm from './components/search-form.vue'
 
 export default {
   name: 'App',
+
   components: {
+    SearchForm,
   }
 }
 </script>
+
+
+<style>
+/* Box sizing rules */
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+/* Remove default margin */
+body,
+h1,
+h2,
+h3,
+h4,
+p,
+ul,
+ol,
+li,
+figure,
+figcaption,
+blockquote,
+fieldset,
+dl,
+dd {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Roboto', sans-serif;
+}
+
+button,
+input,
+select,
+fieldset {
+  outline: none;
+  border: none;
+}
+
+.row {
+  display: flex;
+  flex: 1;
+}
+
+</style>
---
+++ src/components/UI/v-switch.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="v-switch">
+    <span
+        class="v-switch-wrapper"
+        role="checkbox"
+        tabindex="0"
+        @click="toggle"
+        @keydown.space.prevent="toggle"
+    >
+      <span
+        class="v-switch-background"
+      />
+      <span
+        class="v-switch-indicator"
+        :style="indicatorStyles"
+      />
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'v-switch',
+
+  props: {
+    typeSearch:{
+      type: Boolean,
+      default: true
+    }
+  },
+
+  computed: {
+    indicatorStyles() {
+      return { transform: this.typeSearch ? 'translateX(20px)' : 'translateX(0)' };
+    }
+  },
+
+  methods: {
+    toggle() {
+      this.$emit('change-type-search', !this.typeSearch);
+    }
+  }
+};
+</script>
+
+<style>
+.v-switch {
+  margin: 0 12px;
+}
+
+.v-switch-wrapper {
+  display: inline-block;
+  position: relative;
+  cursor: pointer;
+  width: 40px;
+  height: 24px;
+  border-radius: 9999px;
+}
+
+.v-switch-wrapper:focus {
+  outline: 0;
+}
+
+.v-switch-background {
+  display: inline-block;
+  border-radius: 9999px;
+  height: 100%;
+  width: 100%;
+  box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: background-color .4s ease;
+  background-color: #3d9967;
+}
+
+.v-switch-indicator {
+  position: absolute;
+  height: 17px;
+  width: 17px;
+  left: 2px;
+  bottom: 4px;
+  background-color: #ffffff;
+  border-radius: 9999px;
+  box-shadow:  0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: transform .4s ease;
+}
+</style>
